b9d0213df06a779c4c91da2ad3ae69d11f8bbe2b
Valerii Hrokhotskyi <waleryvvs1@gmail.com>
2025-02-02 22:54:41 +0200

    Rework project workspace

---
+++ crates/sdk/src/main.rs
@@ -0,0 +1,256 @@
+use bevy::prelude::*;
+use bevy_asset::{ReflectAsset, UntypedAssetId};
+use bevy_egui::{EguiContext, EguiContextSettings, EguiPostUpdateSet};
+use bevy_inspector_egui::bevy_inspector::hierarchy::{hierarchy_ui, SelectedEntities};
+use bevy_inspector_egui::bevy_inspector::{
+    self, ui_for_entities_shared_components, ui_for_entity_with_children,
+};
+use bevy_inspector_egui::DefaultInspectorConfigPlugin;
+use camera::{camera_movement, SdkCamera};
+use std::any::TypeId;
+use bevy::ecs::observer::TriggerTargets;
+use bevy::picking::backend::PointerHits;
+use bevy::picking::focus::HoverMap;
+use bevy::picking::pointer::{PointerAction, PointerInput, PointerMap};
+use bevy::picking::pointer::PointerAction::Pressed;
+use bevy_infinite_grid::{InfiniteGridBundle, InfiniteGridPlugin};
+// use bevy_mod_picking::backends::egui::EguiPointer;
+// use bevy_mod_picking::prelude::*;
+use bevy_reflect::TypeRegistry;
+use bevy_render::camera::{CameraProjection, Viewport};
+use bevy_window::{PresentMode, PrimaryWindow, Window, WindowMode, WindowTheme};
+use egui_dock::{DockArea, DockState, NodeIndex, Style};
+use transform_gizmo_bevy::{GizmoCamera, GizmoHotkeys, GizmoOptions, GizmoTarget, TransformGizmoPlugin};
+#[cfg(egui_dock_gizmo)]
+use transform_gizmo_egui::GizmoMode;
+use crate::editor_commands::{handle_input, HistoryManager};
+use crate::gizmo::draw_gizmo;
+use crate::ui::{show_ui_system, UiState};
+use crate::ui::camera_viewport::set_camera_viewport;
+
+/// Placeholder type if gizmo is disabled.
+#[cfg(not(egui_dock_gizmo))]
+#[derive(Clone, Copy)]
+struct GizmoMode;
+
+
+mod camera;
+mod gizmo;
+mod domain;
+mod editor_commands;
+mod ui;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "RRay SDK".into(),
+                ..default()
+            }),
+            ..default()
+        }))
+        // .add_plugins(bevy_framepace::FramepacePlugin) // reduces input lag
+        .add_plugins(DefaultInspectorConfigPlugin)
+        .add_plugins(MeshPickingPlugin)
+        .add_plugins(bevy_egui::EguiPlugin)
+        .add_plugins(TransformGizmoPlugin)
+        .add_plugins(InfiniteGridPlugin)
+        // .add_plugins(bevy_mod_picking::plugins::DefaultPickingPlugins)
+        .insert_resource(UiState::new())
+        .insert_resource(HistoryManager::new())
+        .add_systems(Startup, (init_window, setup).chain())
+        .add_systems(
+            PostUpdate,
+            show_ui_system
+                .before(EguiPostUpdateSet::ProcessOutput)
+                .before(bevy_egui::end_pass_system)
+                .before(TransformSystem::TransformPropagate),
+        )
+        .add_systems(PostUpdate, set_camera_viewport.after(show_ui_system))
+        .add_systems(Update, (
+            draw_gizmo, 
+            camera_movement,
+            handle_input,
+            pick_system
+        ))
+        .insert_resource(GizmoOptions {
+            hotkeys: Some(GizmoHotkeys {
+                enable_snapping: Some(KeyCode::ControlLeft),
+                enable_accurate_mode: None,
+                toggle_rotate: None,
+                toggle_translate: None,
+                toggle_scale: None,
+                toggle_x: None,
+                toggle_y: None,
+                toggle_z: None,
+                deactivate_gizmo: None,
+                ..default()
+            }),
+            // snapping: true,
+            ..default()
+        })
+        .register_type::<SdkCamera>()
+        .register_type::<Option<Handle<Image>>>()
+        .register_type::<AlphaMode>()
+        .run();
+}
+
+pub fn pick_system(
+    mouse_events: Res<ButtonInput<MouseButton>>,
+    hover_map: Res<HoverMap>,
+    mut ui_state: ResMut<UiState>,
+) {
+    if mouse_events.just_pressed(MouseButton::Left) {
+        for (_pointer, pointer_map) in hover_map.iter() {
+            println!("{:?}", pointer_map);
+            let option = pointer_map.iter().next();
+            if let Some((entity, target)) = option {
+                println!("{:?} -> {:?}", _pointer, entity);
+                ui_state.selected_entities.select_replace(entity.clone())
+            }
+        }
+    }
+}
+
+#[derive(Component)]
+struct MainCamera;
+
+
+fn init_window(mut query: Query<&mut Window>) {
+    for mut window in query.iter_mut() {
+        window.set_maximized(true)
+    }
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    commands.spawn(InfiniteGridBundle::default());
+
+    let box_size = 2.0;
+    let box_thickness = 0.15;
+    let box_offset = (box_size + box_thickness) / 2.0;
+
+    // left - red
+    let mut transform = Transform::from_xyz(-box_offset, box_offset, 0.0);
+    transform.rotate(Quat::from_rotation_z(std::f32::consts::FRAC_PI_2));
+
+    commands.spawn((
+        Mesh3d(meshes.add(Cuboid::new(box_size, box_thickness, box_size))),
+        MeshMaterial3d(materials.add(StandardMaterial {
+            base_color: Color::srgb(0.63, 0.065, 0.05),
+            ..Default::default()
+        })),
+        transform,
+    ));
+    // right - green
+    let mut transform = Transform::from_xyz(box_offset, box_offset, 0.0);
+    transform.rotate(Quat::from_rotation_z(std::f32::consts::FRAC_PI_2));
+    commands.spawn((
+        Mesh3d(meshes.add(Cuboid::new(box_size, box_thickness, box_size))),
+        transform,
+        MeshMaterial3d(materials.add(StandardMaterial {
+            base_color: Color::srgb(0.14, 0.45, 0.091),
+            ..Default::default()
+        })),
+    ));
+    // bottom - white
+    commands.spawn((
+        Mesh3d(meshes.add(Cuboid::new(
+            box_size + 2.0 * box_thickness,
+            box_thickness,
+            box_size,
+        ))),
+        MeshMaterial3d(materials.add(StandardMaterial {
+            base_color: Color::srgb(0.725, 0.71, 0.68),
+            ..Default::default()
+        })),
+    ));
+    // top - white
+    let transform = Transform::from_xyz(0.0, 2.0 * box_offset, 0.0);
+    commands.spawn((
+        Mesh3d(meshes.add(Cuboid::new(
+            box_size + 2.0 * box_thickness,
+            box_thickness,
+            box_size,
+        ))),
+        transform,
+        MeshMaterial3d(materials.add(StandardMaterial {
+            base_color: Color::srgb(0.725, 0.71, 0.68),
+            ..Default::default()
+        })),
+    ));
+    // back - white
+    let mut transform = Transform::from_xyz(0.0, box_offset, -box_offset);
+    transform.rotate(Quat::from_rotation_x(std::f32::consts::FRAC_PI_2));
+    commands.spawn((
+        Mesh3d(meshes.add(Cuboid::new(
+            box_size + 2.0 * box_thickness,
+            box_thickness,
+            box_size + 2.0 * box_thickness,
+        ))),
+        transform,
+        MeshMaterial3d(materials.add(StandardMaterial {
+            base_color: Color::srgb(0.725, 0.71, 0.68),
+            ..Default::default()
+        })),
+    ));
+
+    // ambient light
+    commands.insert_resource(AmbientLight {
+        color: Color::WHITE,
+        brightness: 0.02,
+    });
+    // top light
+    commands
+        .spawn((
+            Mesh3d(meshes.add(Plane3d::default().mesh().size(0.4, 0.4))),
+            Transform::from_matrix(Mat4::from_scale_rotation_translation(
+                Vec3::ONE,
+                Quat::from_rotation_x(std::f32::consts::PI),
+                Vec3::new(0.0, box_size + 0.5 * box_thickness, 0.0),
+            )),
+            MeshMaterial3d(materials.add(StandardMaterial {
+                base_color: Color::WHITE,
+                emissive: LinearRgba::WHITE * 100.0,
+                ..Default::default()
+            })),
+        ))
+        .with_children(|builder| {
+            builder.spawn((
+                PointLight {
+                    color: Color::WHITE,
+                    intensity: 25000.0,
+                    ..Default::default()
+                },
+                Transform::from_translation((box_thickness + 0.05) * Vec3::Y),
+            ));
+        });
+
+
+
+    // directional light
+    commands.spawn((
+        DirectionalLight {
+            illuminance: 2000.0,
+            ..default()
+        },
+        Transform::from_rotation(Quat::from_rotation_x(-std::f32::consts::PI / 2.0)),
+    ));
+
+    // camera
+    commands.spawn((
+        Name::new("Camera"),
+        Camera3d::default(),
+        Transform::from_xyz(0.0, box_offset, 4.0)
+            .looking_at(Vec3::new(0.0, box_offset, 0.0), Vec3::Y),
+        MainCamera,
+        SdkCamera::default(),
+        GizmoCamera,
+        RayCastPickable
+        // PickRaycastSource,
+    ));
+}
+
---
+++ crates/sdk/src/ui/mod.rs
@@ -0,0 +1,162 @@
+pub mod asset;
+pub mod camera_viewport;
+pub mod resource;
+
+use bevy::prelude::{AppTypeRegistry, Resource, With, World};
+use bevy_asset::UntypedAssetId;
+use bevy_inspector_egui::bevy_inspector;
+use bevy_inspector_egui::bevy_inspector::hierarchy::{hierarchy_ui, SelectedEntities};
+use bevy_inspector_egui::bevy_inspector::{
+    ui_for_entities_shared_components, ui_for_entity_with_children,
+};
+use egui_dock::{DockArea, DockState, NodeIndex, Style};
+use std::any::TypeId;
+use bevy_egui::EguiContext;
+use bevy_window::PrimaryWindow;
+use crate::GizmoMode;
+use crate::ui::asset::select_asset;
+use crate::ui::resource::select_resource;
+
+#[derive(Debug)]
+pub enum EguiWindow {
+    GameView,
+    Hierarchy,
+    Resources,
+    Assets,
+    Inspector,
+}
+
+
+
+#[derive(Resource)]
+pub struct UiState {
+    pub state: DockState<EguiWindow>,
+    pub viewport_rect: egui::Rect,
+    pub selected_entities: SelectedEntities,
+    pub selection: InspectorSelection,
+    pub gizmo_mode: GizmoMode,
+}
+
+#[derive(Eq, PartialEq)]
+pub enum InspectorSelection {
+    Entities,
+    Resource(TypeId, String),
+    Asset(TypeId, String, UntypedAssetId),
+}
+
+pub struct TabViewer<'a> {
+    world: &'a mut World,
+    selected_entities: &'a mut SelectedEntities,
+    selection: &'a mut InspectorSelection,
+    viewport_rect: &'a mut egui::Rect,
+    gizmo_mode: GizmoMode,
+}
+
+impl egui_dock::TabViewer for TabViewer<'_> {
+    type Tab = EguiWindow;
+
+    fn ui(&mut self, ui: &mut egui_dock::egui::Ui, window: &mut Self::Tab) {
+        let type_registry = self.world.resource::<AppTypeRegistry>().0.clone();
+        let type_registry = type_registry.read();
+
+        match window {
+            EguiWindow::GameView => {
+                *self.viewport_rect = ui.clip_rect();
+
+                // draw_gizmo(ui, self.world, self.selected_entities, self.gizmo_mode);
+            }
+            EguiWindow::Hierarchy => {
+                let selected = hierarchy_ui(self.world, ui, self.selected_entities);
+                if selected {
+                    *self.selection = InspectorSelection::Entities;
+                }
+            }
+            EguiWindow::Resources => select_resource(ui, &type_registry, self.selection),
+            EguiWindow::Assets => select_asset(ui, &type_registry, self.world, self.selection),
+            EguiWindow::Inspector => match *self.selection {
+                InspectorSelection::Entities => match self.selected_entities.as_slice() {
+                    &[entity] => ui_for_entity_with_children(self.world, entity, ui),
+                    entities => ui_for_entities_shared_components(self.world, entities, ui),
+                },
+                InspectorSelection::Resource(type_id, ref name) => {
+                    ui.label(name);
+                    bevy_inspector::by_type_id::ui_for_resource(
+                        self.world,
+                        type_id,
+                        ui,
+                        name,
+                        &type_registry,
+                    )
+                }
+                InspectorSelection::Asset(type_id, ref name, handle) => {
+                    ui.label(name);
+                    bevy_inspector::by_type_id::ui_for_asset(
+                        self.world,
+                        type_id,
+                        handle,
+                        ui,
+                        &type_registry,
+                    );
+                }
+            },
+        }
+    }
+
+    fn title(&mut self, window: &mut Self::Tab) -> egui_dock::egui::WidgetText {
+        format!("{window:?}").into()
+    }
+
+    fn clear_background(&self, window: &Self::Tab) -> bool {
+        !matches!(window, EguiWindow::GameView)
+    }
+}
+
+impl UiState {
+    pub fn new() -> Self {
+        let mut state = DockState::new(vec![EguiWindow::GameView]);
+        let tree = state.main_surface_mut();
+        let [game, _inspector] =
+            tree.split_right(NodeIndex::root(), 0.75, vec![EguiWindow::Inspector]);
+        let [game, _hierarchy] = tree.split_left(game, 0.2, vec![EguiWindow::Hierarchy]);
+        let [_game, _bottom] =
+            tree.split_below(game, 0.8, vec![EguiWindow::Resources, EguiWindow::Assets]);
+
+        Self {
+            state,
+            selected_entities: SelectedEntities::default(),
+            selection: InspectorSelection::Entities,
+            viewport_rect: egui::Rect::NOTHING,
+            #[cfg(egui_dock_gizmo)]
+            gizmo_mode: GizmoMode::Translate,
+            #[cfg(not(egui_dock_gizmo))]
+            gizmo_mode: GizmoMode,
+        }
+    }
+
+    fn ui(&mut self, world: &mut World, ctx: &mut egui::Context) {
+        let mut tab_viewer = TabViewer {
+            world,
+            viewport_rect: &mut self.viewport_rect,
+            selected_entities: &mut self.selected_entities,
+            selection: &mut self.selection,
+            gizmo_mode: self.gizmo_mode,
+        };
+        DockArea::new(&mut self.state)
+            .style(Style::from_egui(ctx.style().as_ref()))
+            .show(ctx, &mut tab_viewer);
+    }
+}
+
+pub fn show_ui_system(world: &mut World) {
+    let Ok(egui_context) = world
+        .query_filtered::<&mut EguiContext, With<PrimaryWindow>>()
+        .get_single(world)
+    else {
+        return;
+    };
+    let mut egui_context = egui_context.clone();
+
+    world.resource_scope::<UiState, _>(|world, mut ui_state| {
+        ui_state.ui(world, egui_context.get_mut())
+    });
+}
